770ab1fb825ad690e8d21a09c20b88b6b2bee625
Das <290692@cognizant.com>
2019-03-19 09:23:43 +0100

    initial commit

---
+++ src/app/add-task/add-task.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+export class AddTaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NgbModule, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
+import { UpdateTaskComponent } from './update-task/update-task.component';
+
+const appRoutes: Routes = [
+  { path: 'addTask', component: AddTaskComponent },
+  { path: 'viewTask',      component: ViewTaskComponent },
+  { path: 'updateTask', component: UpdateTaskComponent },
+  { path: '', redirectTo: '/addTask', pathMatch: 'full' }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    ViewTaskComponent,
+    UpdateTaskComponent
   ],
   imports: [
+    NgbModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule
   ],
